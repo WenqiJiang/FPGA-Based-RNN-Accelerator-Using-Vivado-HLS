0e01ee6658b4f8c343a9a673ba07cd931ec6ca0b
WenqiJiang <wenqi.j@columbia.edu>
2019-03-22 22:34:48 -0400

    label incorrect result info

--- main.c
+++ main.c
@@ -42,8 +42,6 @@ int main(int argc, char *argv[])
     float* fc_output_feature_map = malloc(sizeof(float) * FC_BATCH_SIZE * FC_OUTPUT_SIZE);
     float* softmax_result = malloc(sizeof(float) * SM_BATCH_SIZE * SM_CLASS_SIZE);
     int* argmax_result = malloc(sizeof(int) * SM_BATCH_SIZE);
-    /* initialize last state to 0 */
-    zero_init(rnn_last_state, RNN_BATCH_SIZE * RNN_STATE_SIZE);
 
     /* load model in */
     load_float("./model/embedding_1_embeddings.txt", WORD_NUM * WORD_SIZE, word_embedding);
@@ -54,54 +52,58 @@ int main(int argc, char *argv[])
     load_float("./model/dense_1_bias.txt", FC_OUTPUT_SIZE, fc_bias);
     load_float("./model/dense_1_kernel.txt", FC_INPUT_SIZE * FC_OUTPUT_SIZE, fc_kernel);
 
-    // print_float(word_embedding, WORD_SIZE);
-    // print_float(word_embedding + WORD_SIZE, WORD_SIZE);
     /* load dataset in */
     #define SAMPLE_NUM 1000
     #define SAMPLE_LEN 50
     int* sequences = malloc(sizeof(int) * SAMPLE_LEN * SAMPLE_NUM);
-    int* rnn_result = malloc(sizeof(int) * SAMPLE_NUM);
+    int* C_result = malloc(sizeof(int) * SAMPLE_NUM);
+    int* Keras_result = malloc(sizeof(int) * SAMPLE_NUM);
     load_int("./datasets/org_seq.txt", SAMPLE_LEN * SAMPLE_NUM, sequences);
-    load_int("./datasets/rnn_result.txt", SAMPLE_NUM, rnn_result);
+    load_int("./datasets/rnn_result.txt", SAMPLE_NUM, Keras_result);
 
     /* do inference and print the result */
-    #define SEQ_LEN 50
-    for (int i = 0; i < SEQ_LEN; i++) {
-        for (int j = 0; j < RNN_BATCH_SIZE; j++) {
-            int sample_index = j * SEQ_LEN + i;
-            int word_index = sequences[sample_index];
-            // printf("%d\n", word_index);
-            // int word_index = i + 1;
-            int word_embedding_index = word_index * WORD_SIZE;
-            int rnn_input_state_index = j * RNN_INPUT_SIZE;
-            copy_float(&word_embedding[word_embedding_index], 
-                       &rnn_input_state[rnn_input_state_index], RNN_INPUT_SIZE);
+    for (int compute_time = 0; compute_time < SAMPLE_NUM / FC_BATCH_SIZE; compute_time++) {
+        /* initialize last state to 0 */
+        zero_init(rnn_last_state, RNN_BATCH_SIZE * RNN_STATE_SIZE);
+        for (int i = 0; i < SAMPLE_LEN; i++) {
+            for (int j = 0; j < RNN_BATCH_SIZE; j++) {
+                int sample_index = compute_time * SAMPLE_LEN * RNN_BATCH_SIZE + j * SAMPLE_LEN + i;
+                int word_index = sequences[sample_index];
+                int word_embedding_index = word_index * WORD_SIZE;
+                int rnn_input_state_index = j * RNN_INPUT_SIZE;
+                copy_float(&word_embedding[word_embedding_index], 
+                        &rnn_input_state[rnn_input_state_index], RNN_INPUT_SIZE);
+            }
+
+            rnn(rnn_last_state, rnn_input_state, rnn_bias, rnn_kernel, rnn_recurrent_kernel, rnn_output_state);
+            act_tanh(rnn_output_state, RNN_BATCH_SIZE * RNN_STATE_SIZE);
+            float* temp = rnn_last_state;
+            rnn_last_state = rnn_output_state;
+            rnn_output_state = temp;
         }
-
-        // if (i == SEQ_LEN - 1)
-        //     print_float(rnn_last_state, RNN_BATCH_SIZE * RNN_STATE_SIZE);
-        // print_float(rnn_input_state, RNN_BATCH_SIZE * RNN_INPUT_SIZE);
-        rnn(rnn_last_state, rnn_input_state, rnn_bias, rnn_kernel, rnn_recurrent_kernel, rnn_output_state);
-        act_tanh(rnn_output_state, RNN_BATCH_SIZE * RNN_STATE_SIZE);
-        // if (i == SEQ_LEN - 1)
-        //     print_float(rnn_output_state, RNN_BATCH_SIZE * RNN_STATE_SIZE);
-        float* temp = rnn_last_state;
-        rnn_last_state = rnn_output_state;
-        rnn_output_state = temp;
-
-        // if (i == 0)
-        //     print_float(rnn_last_state, RNN_BATCH_SIZE * RNN_STATE_SIZE);
+        /* no ReLu after FC layer, only softmax */
+        fc(rnn_last_state, fc_bias, fc_kernel, fc_output_feature_map);
+        // act_relu(fc_output_feature_map, FC_BATCH_SIZE * FC_OUTPUT_SIZE);
+        softmax(fc_output_feature_map, softmax_result);
+        // print_float(softmax_result, SM_CLASS_SIZE);
+        argmax(fc_output_feature_map, argmax_result);
+        copy_int(argmax_result, &C_result[compute_time * RNN_BATCH_SIZE], RNN_BATCH_SIZE);
+        // print_int(argmax_result, SM_BATCH_SIZE);
+        for (int i = compute_time * RNN_BATCH_SIZE; i < (compute_time + 1) * RNN_BATCH_SIZE; i++) {
+            if (C_result[i] == Keras_result[i])
+                printf("Sample %d:\t result: %d\n", i, C_result[i]);
+            else {
+                printf("Sample %d:\t C_result: %d\t Keras_result: %d\t P(%d): %f\t P(%d): %f\n", 
+                i, C_result[i], Keras_result[i], 
+                C_result[i], softmax_result[(i - compute_time * RNN_BATCH_SIZE) * SM_CLASS_SIZE + C_result[i]],
+                Keras_result[i], softmax_result[(i - compute_time * RNN_BATCH_SIZE) * SM_CLASS_SIZE + Keras_result[i]]);
+            }           
+        }
     }
-    // print_float(rnn_last_state, RNN_STATE_SIZE);
-    fc(rnn_last_state, fc_bias, fc_kernel, fc_output_feature_map);
-    // act_relu(fc_output_feature_map, FC_BATCH_SIZE * FC_OUTPUT_SIZE);
-    softmax(fc_output_feature_map, softmax_result);
-    print_float(softmax_result, SM_CLASS_SIZE);
-    argmax(fc_output_feature_map, argmax_result);
-    print_int(argmax_result, SM_BATCH_SIZE);
+    
     printf("%s", "Press any key then ENTER to end the program\n");
     char str2[10];
-    scanf("%s", str2);
+    // scanf("%s", str2);
 
     return 0;
 }
